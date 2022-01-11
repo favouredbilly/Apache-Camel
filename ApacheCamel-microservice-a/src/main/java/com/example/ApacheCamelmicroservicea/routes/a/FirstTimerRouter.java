f8726a9e873425b28c92a41fb596a89d6f02d0fe
Jimoh <Biliaminu.Jimoh@landisgyr.com>
2022-01-11 14:38:15 +0200

    creating timer and logging the current time in a log

--- ApacheCamel-microservice-a/src/main/java/com/example/ApacheCamelmicroservicea/routes/a/FirstTimerRouter.java
+++ ApacheCamel-microservice-a/src/main/java/com/example/ApacheCamelmicroservicea/routes/a/FirstTimerRouter.java
@@ -1,5 +1,26 @@
 package com.example.ApacheCamelmicroservicea.routes.a;
 
-public class FirstTimerRouter {
+import java.time.LocalDateTime;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirstTimerRouter extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+		// TODO Auto-generated method stub
+		//queue--timer
+		//transformation
+		//database--log
+		//
+		from("timer:first-timer")//queue
+		//.transform().constant("My Constnat message")
+		.transform().constant("Time now is " + LocalDateTime.now())
+		.to("log:first-timer");//database
+		
+		
+	}
 
 }
