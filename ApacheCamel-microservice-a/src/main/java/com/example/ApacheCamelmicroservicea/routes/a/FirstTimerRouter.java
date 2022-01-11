5a7c50eaf2268783823142a05ad8f872e6210a42
Jimoh <Biliaminu.Jimoh@landisgyr.com>
2022-01-11 14:14:51 +0200

    apache caramel create two microservices

---
+++ ApacheCamel-microservice-a/src/main/java/com/example/ApacheCamelmicroservicea/routes/a/FirstTimerRouter.java
@@ -0,0 +1,5 @@
+package com.example.ApacheCamelmicroservicea.routes.a;
+
+public class FirstTimerRouter {
+
+}
---
+++ ApacheCamel-microservice-b/src/test/java/com/example/ApacheCamelmicroserviceb/ApacheCamelMicroserviceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApacheCamelmicroserviceb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheCamelMicroserviceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
